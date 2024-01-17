fbf567d124419accbb1388ed4d31c3d69ab51dbf
eddington524 <iamsam8687@gmail.com>
2024-01-17 23:59:53 +0900

    feat: BookViewController

---
+++ network_assignment/View/Cell/BookCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  BookCollectionViewCell.swift
+//  network_assignment
+//
+//  Created by Sammy Jung on 2024/01/17.
+//
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
